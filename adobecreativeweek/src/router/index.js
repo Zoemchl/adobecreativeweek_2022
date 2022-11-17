05af1b01cd91956de15a27d1a970daa215b187ed
Zoé <zoe.michel@edu.devinci.fr>
2022-11-17 15:12:22 +0100

    route bug resolved

--- adobecreativeweek/src/router/index.js
+++ adobecreativeweek/src/router/index.js
@@ -1,24 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Game from '../views/Game.vue'
 import Pres from '../views/Pres.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home', 
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-    },
-    {
-      path: '/pres',
       name: 'pres',
-      component:'Pres'
+      component:Pres 
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component:Game
     }
   ]
 })
---
+++ adobecreativeweek/src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+    <Chatbox />
+</template>
+
+<script>
+import Chatbox from "../components/Chatbox.vue";
+
+export default{
+    name: 'game',
+    components: { Chatbox},
+}
+</script>
+
+<style scoped>
+</style>
