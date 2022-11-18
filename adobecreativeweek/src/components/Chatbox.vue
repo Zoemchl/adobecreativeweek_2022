da1deea24e03d193e0ba579052dd647480adf0be
Alexis Bally <alexisbally02@gmail.com>
2022-11-18 02:25:38 +0100

    Add messages

--- adobecreativeweek/src/components/Chatbox.vue
+++ adobecreativeweek/src/components/Chatbox.vue
@@ -2,9 +2,47 @@
 
     <div class="chatbox">
         <div class="container">
-            <p id="message1" class="chat active">> Salut !</p>
-            <p id="message2" class="chat">> Test !</p>
-            <p id="message3" class="chat">> 25456 !</p>
+            <p id="message1" class="chat active">> Salut ! Moi, c’est IIMERSIVE.
+                <br>
+                > Je suis *Bzz bip bop bop*
+                <br>
+                > Qu’est ce qu’il...*bip* m’arrive ?!</p>
+            <p id="message2" class="chat">> Il semblerait qu’un virus ai endommagé mon système. 
+                <br>
+                > Des parties de ma mémoire ont comme... disparu !</p>
+            <p id="message3" class="chat">> Sans ma mémoire, je ne peux pas accomplir ma mission, qui est de guider les futurs étudiants à la découverte de l’IIM !</p>
+            <p id="message4" class="chat">> J’ai besoin de ton aide pour me réparer ! 
+                <br>
+                > Mais comment *booop* allons-nous faire ?</p>
+            <p id="message5" class="chat">> J’ai une idée !
+                <br>
+                > *Bzzz bilip*</p>
+            <p id="message6" class="chat">> Nous allons parcourir l’école à la recherche des différentes salles correspondant à chaque axe.</p>
+            <p id="message7" class="chat">> Avec les informations collectés, je pourrai peut-être rétablir mes connexions neuronales électroniques !</p>
+            <p id="message8" class="chat">> Regarde là bas ! 
+                <br>
+                > On peut déjà apercevoir la salle de 3D !</p>
+            <p id="message9" class="chat">> Hmmm étrange...</p>
+            <p id="message10" class="chat">> Cela ressemble aux plans 3D de mes confrères.
+                <br>
+                > Essaye de manipuler ces pièces pour trouver quelque chose...</p>
+            <p id="message11" class="chat">> Dès que tu penses avoir obtenu une réponse, introduit la dans mon terminal.</p>
+            <p id="message12" class="chat">> Bien joué !
+                <br>
+                > Prenons cette porte et découvrons quelle salle se cache derrière...</p>
+            <p id="message13" class="chat">> Oh du code partout ! C'est ma langue maternelle
+                <br>
+                > Nous sommes chez les Coding & Digital Innovation</p>
+            <p id="message14" class="chat">> Je détecte la présence d'un lien dans mon système, clique dessus !
+                <br>
+                > Alors tu as trouvé le code secret ?</p>
+            <p id="message14" class="chat">> Je détecte la présence d'un lien dans mon système, clique dessus !</p>
+            <p id="message15" class="chat">> Alors tu as trouvé le code secret ?</p>
+            <p id="message16" class="chat">> Cette journée d'intégration a l'air vraiment chouette 
+                <br>
+                > Restons dans le thème du divertissement et visitons la salle des Jeux Vidéos </p>
+            <p id="message17" class="chat">> Génial ! Tu vas pouvoir tester ce jeu</p>
+            <p id="message18" class="chat">> Quel personnage de qualité tu viens de créer !</p>
             <input placeholder="Le code doit contenir 8 chiffres" id="answer" type="number">
             <button @click="check_answer()">Vérifier votre réponse</button>
             <img class="happy_iimersive" src="../assets/img/happy_iimersive.png" alt="IIMersive happy face icon">
